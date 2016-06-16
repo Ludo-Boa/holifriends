fd1ef2d91111129b62f688f157a991489bb79915
Nico <nicolascapron403@gmail.com>
2016-06-16 22:58:56 +0200

    fos fixed

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -1,3 +1,45 @@
 <?php
 
-namespace UserBundle\Entity\
+// src/AppBundle/Entity/User.php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+
+* @ORM\Entity
+
+* @ORM\Table("fos_user")
+
+*/
+
+class User extends BaseUser
+
+{
+
+/**
+
+* @ORM\Id
+
+* @ORM\Column(type="integer")
+
+* @ORM\GeneratedValue(strategy="AUTO")
+
+*/
+
+protected $id;
+
+public function __construct()
+
+{
+
+parent::__construct();
+
+// your own logic
+
+}
+
+}
